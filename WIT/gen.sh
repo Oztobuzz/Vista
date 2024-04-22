0b593e387a91b98eaee8d422ce1ecd24924e4b4f
hopbv <hopbv@vng.com.vn>
2024-04-22 15:01:04 +0700

    update script WIT

--- WIT/gen.sh
+++ WIT/gen.sh
@@ -1,6 +1,29 @@
+# Train
 python wit_main.py \
     --subset train \
     --dataset-path data/vi_wit_v1.train.filtered.json \
+    --prompt-folder prompts \
+    --output-path wit_output \
+    --model-name models/gemini-1.0-pro-vision-latest \
+    --max-output-tokens 16000 \
+    --temperature 0.0 \
+    --api-key-path api-key.txt
+
+# Validation
+python wit_main.py \
+    --subset val \
+    --dataset-path data/vi_wit_v1.val.filtered.json \
+    --prompt-folder prompts \
+    --output-path wit_output \
+    --model-name models/gemini-1.0-pro-vision-latest \
+    --max-output-tokens 16000 \
+    --temperature 0.0 \
+    --api-key-path api-key.txt
+
+# Test
+python wit_main.py \
+    --subset test \
+    --dataset-path data/vi_wit_v1.test.filtered.json \
     --prompt-folder prompts \
     --output-path wit_output \
     --model-name models/gemini-1.0-pro-vision-latest \
