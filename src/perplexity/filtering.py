4c4477cf2bd8cdc5d11a656f95c39b6f42b9f438
pphuc25 <phanphuc1100@gmail.com>
2024-05-14 15:15:49 +0700

    fix: rename filtering to perplexity, remove unused feature in class

--- src/perplexity/filtering.py
+++ src/perplexity/filtering.py
@@ -8,14 +8,13 @@ from datasets import Dataset
 from .document_modification import ModifyingDocuments
 
 class FilteringPerplexity:
-    def __init__(self, dataset, sentencepiece_model_path, kenlm_model_path, num_proc):
-        self.dataset = dataset
+    def __init__(self, sentencepiece_model_path, kenlm_model_path, num_proc):
         self.sentencepiece_model = load_sentencepiece_model(sentencepiece_model_path)
         self.kenlm_model = load_kenlm_model(kenlm_model_path)
         self.num_proc = num_proc
 
-    def compute(self, conversation_feature_name="conversation", content_feature_name="content"):
-        dataset = self.dataset.map(
+    def compute(self, dataset: Dataset, conversation_feature_name="conversation", content_feature_name="content"):
+        dataset = dataset.map(
             lambda examples: self._compute_perplexity(
                 examples, conversation_feature_name, content_feature_name
             ), num_proc=self.num_proc)
@@ -24,7 +23,7 @@ class FilteringPerplexity:
     @staticmethod
     def filter(dataset: Dataset, threshold):
         dataset = dataset.filter(lambda example: example["perplexity"] < threshold)
-        return 
+        return dataset
 
     def _compute_perplexity(self, example, conversation_feature_name="conversation", content_feature_name="content"):
         check_document = example[conversation_feature_name]
